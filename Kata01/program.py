0f8faed7bd1bd5621dca6237dac159470d078d1e
AramisRamirezITT <77717380+AramisRamirezITT@users.noreply.github.com>
2022-02-16 21:34:40 -0700

    First

---
+++ Kata01/program.py
@@ -0,0 +1,41 @@
+# program.py
+sum = 1 + 2
+print(sum)
+
+# Impresión en pantalla print('Hola desde la consola')
+print('Hola desde la consola')
+
+# Tu turno, prueba el fragmento de código anterior
+
+sum = 1 + 2 # 3
+product = sum * 2
+# print(product)
+
+# Declaramos la variable
+distancia_a_alfa_centauri = 4.367
+
+# Descubrimos su tipo de dato
+# print (type(distancia_a_alfa_centauri))
+
+# Importamos la biblioteca 
+from datetime import date
+
+# Obtenemos la fecha de hoy
+# date.today()
+
+# Mostramos la fecha en la consola
+# print(date.today())
+
+# Tu turno ejecuta el siguiente comando: print("Today's date is: " + str(date.today()))
+print("Today's date is: " + str(date.today()))
+
+
+# Escribe tu código aquí
+# print("Bienvenido al programa de bienvenida")
+# name = input("Introduzca su nombre ")
+# print("Saludos: " + name)
+
+print("Calculadora")
+first_number = input("Primer número: ")
+second_number = input("Segundo número: ")
+print(first_number + second_number)
