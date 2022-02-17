f29a401cedcf39e0555f2cc042489821a2404001
AramisRamirezITT <77717380+AramisRamirezITT@users.noreply.github.com>
2022-02-16 21:38:50 -0700

    Commit Kata02
    Se agrego el ENV como zip porque no permite add mas de 100 files

---
+++ Kata02/kata02.py
@@ -0,0 +1,17 @@
+""" 
+Creacion de un ENV 
+
+python3 -m venv env 
+
+source env/bin/activate
+# Windows
+env\bin\activate
+
+o bien: 
+env\Scripts\activate ***Esta es la que funciona  
+
+# Linux, WSL or macOS
+source env/bin/activate
+
+
+"""
